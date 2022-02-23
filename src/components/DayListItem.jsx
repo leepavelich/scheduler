b73a6dc65a31ff097c2732a8bd824f47b69e4ad8
leepavelich <leepavelich@MacBook-Pro.local>
2022-02-23 11:37:18 -0700

    Implement DayListItem

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -4,18 +4,20 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const dayClass = {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots,
+  });
+
+  const formatSpots = () => {
+    if (props.spots > 1) return `${props.spots} spots remaining`;
+    return props.spots ? "1 spot remaining" : "no spots remaining";
   };
 
   return (
-    <li
-      onClick={() => props.setDay(props.name)}
-      className={classNames("day-list__item", dayClass)}
-    >
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
