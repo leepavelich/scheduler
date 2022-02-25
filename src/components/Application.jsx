ad651bae2b32638cdce515d8e6cfa41d0a1210df
leepavelich <leepavelich@MacBook-Pro.local>
2022-02-24 17:44:12 -0700

    Axios now used to GET API request for days

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,28 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
-import DayList from "components/DayList";
 
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
   },
 ];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("api/days").then((response) => setDays(response.data));
+  }, []);
+
+  const schedule = appointments.map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ));
 
   return (
     <main className="layout">
@@ -43,7 +76,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {schedule}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
