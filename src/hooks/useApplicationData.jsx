eb9f2a8d70c86322fe4b21c4e16646ad3c3d44ee
leepavelich <leepavelich@MacBook-Pro.local>
2022-03-01 11:06:51 -0700

    Refactored database promise

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -71,11 +71,12 @@ export default function useApplicationData() {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers"),
     ]).then((all) => {
+      const [days, appointments, interviewers] = all.map((x) => x.data);
       dispatch({
         type: SET_APPLICATION_DATA,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
+        days,
+        appointments,
+        interviewers,
       });
     });
 
