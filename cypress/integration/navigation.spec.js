286b3669742989beedc59025cb2a91d176642915
leepavelich <leepavelich@MacBook-Pro.local>
2022-03-02 12:59:15 -0700

    Expand our E2E cypress testing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,13 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("should visit root", () => {});
+
+  it("should navigate to Tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
